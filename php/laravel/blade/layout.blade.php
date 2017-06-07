a820d887f8a3e5880e6eab15007a8ed1a46bfc86
Jeff Puckett <jeff@jeffpuckett.com>
2017-06-07 20:21:17 +0000

    add Lato font

--- php/laravel/blade/layout.blade.php
+++ php/laravel/blade/layout.blade.php
@@ -9,7 +9,7 @@
 
     <title>{{ config('app.name') }}</title>
     <link rel="shortcut icon" href="{{ razorbacks\style\Manifest::cdn() }}/images/favicon.ico" type="image/x-icon"/>
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/smoothness/jquery-ui.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
